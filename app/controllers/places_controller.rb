a07d1a51288553ab90d123f203035d2702eb0f01
brianneking86 <brianneking86@gmail.com>
2015-01-28 16:57:33 -0500

    udated form to save place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,5 +3,22 @@ class PlacesController < ApplicationController
   end
 
   def create
+    
+    @user = current_user
+    @place = Place.new(location_params)
+    if @place.save
+      redirect_to profile_path(@user)
+    else
+      notice = "Please fill out the form appropriately"
+    end
   end
+
+  private
+    def location_params
+      params.require(:place).permit(:name, :location, :user_id)
+    end
 end
+
+#{"utf8"=>"✓", "authenticity_token"=>"bS4oA3ldH3g/TQZmgO5UnWYgkNgh2YZvU4DaGaqIuPo=", 
+#"place"=>{"name"=>"Paris", "location"=>"France", "user_id"=>"2"}, 
+#"commit"=>"Add", "action"=>"create", "controller"=>"places"}
